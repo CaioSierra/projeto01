6bb1c51458d9f8bd2a12d03cc795bbd82b9e9fc7
CaioSierra <caiosierra@hotmail.com>
2022-10-06 21:13:08 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Projeto02.Controllers;
+using Projeto02.Entities;
+
+namespace Projeto02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Aula 02 - C# WebDeveloper");
+
+            //Instanciando a classe de controle
+            var funcionarioController = new FuncionarioController();
+
+            //Executando o método para cadastrar funcionário
+            funcionarioController.CadastrarFuncionario();
+
+            //Pausar o prompt
+            Console.ReadKey();
+        }
+    }
+}
